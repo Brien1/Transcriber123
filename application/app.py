a6a8ae67f33fabf27c47fed04802947df0be2b0b
40314811 <bhall06@qub.ac.uk>
2022-07-14 22:18:59 +0100

    attempting to get process page working

--- application/app.py
+++ application/app.py
@@ -12,8 +12,7 @@ def index():
 @app.route("/" , methods=['GET', 'POST'])
 def upload_file():
     f= request.files.get("file1")
-    
-    f.save("/Users/brienhall/Documents/FinalProject/app/user_loaded_content/temp")
+    # f.save("/Users/brienhall/Documents/FinalProject/app/user_loaded_content/temp")
     # f.save("/var/www/uploads/uploaded_file")
     
     # request.files[1].save(os.path.join(app.config['UPLOAD_FOLDER'], "uploaded_content"))
