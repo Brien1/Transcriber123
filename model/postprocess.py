f4d47d446ecc11e274fa5655b2f6851a9d8ba135
40314811 <bhall06@qub.ac.uk>
2022-07-15 17:31:46 +0100

    pre and post process

---
+++ model/postprocess.py
@@ -0,0 +1,12 @@
+import base64 as base64
+import matplotlib.pyplot as plt
+def show_predicted_image(y_predict):
+    """Takes the base64 string output from the model and converts it to a image, 
+    then displays it
+
+    Args:
+        y_predict (ndarray): Shape is (1,), a single string contained within a np.array
+    """
+    with open ("new_image.png","bw") as tempwrite:
+        tempwrite.write(base64.b64decode(y_predict))
+    plt.imshow(plt.imread(fname="new_image.png"))
---
+++ model/preprocess.py
@@ -0,0 +1,31 @@
+import librosa
+def resizeaudio(audio, max_l):
+    """Resizes the audio to a certian number of bytes, which is essential for feeding
+    into the .fit() method
+
+    Args:
+        audio (list/ array): where each element is a list of float values reperesenting an image
+        max_l (_type_): True will add trailing zeros (bytes) onto the end of shorter audio samples
+                        False will cut the audio to the matches the shortes in the list
+
+    Returns:
+        _type_: list
+    """
+    audio_lengths = [32702,64000]
+    if max_l :
+        audio = [librosa.util.fix_length(f, max(audio_lengths)) for f in audio]
+    else:
+        audio = [librosa.util.fix_length(f, min(audio_lengths)) for f in audio]
+        
+    return audio
+def loadaudio(path):
+    """_summary_
+
+    Args:
+        path (string): path
+
+    Returns:
+        audio (numpy bytes), sample_rate: returns an object and int, the decoded audio and the int sample rate
+    """
+    
+    return librosa.load(path)
