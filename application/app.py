cda27da4a5ba1170d73f8c2243fa58918cd4077a
40314811 <bhall06@qub.ac.uk>
2022-07-16 21:19:45 +0100

    moved static file, image now shows locally

--- application/app.py
+++ application/app.py
@@ -1,9 +1,12 @@
 import sys
 import os
-from flask import Flask, request, redirect, url_for,render_template,send_from_directory
+from flask import Flask, request, redirect, url_for,render_template,send_from_directory, send_file, abort
 from werkzeug.utils import secure_filename
 import pickle
 import sklearn
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 CURRENTDIR = os.path.dirname(os.path.realpath(__file__))
 PARENTDIR = os.path.dirname(CURRENTDIR)
 sys.path += [PARENTDIR,]
@@ -15,30 +18,45 @@ UPLOAD_FOLDER = "user_loaded_content"
 MYDIR = os.path.dirname(__file__)
 MODEL = os.path.join(PARENTDIR,"model/model.pkl")
 app = Flask(__name__)
-print(app.static_url_path)
-app.static_url_path = PARENTDIR+"/static"
-print(app.static_url_path)
+# print(app.static_url_path)
+# app.static_url_path = PARENTDIR+"/static"
+# print(app.static_url_path)
 app.config["UPLOAD_FOLDER"] = MYDIR + "/user_loaded_content"
 
 @app.route("/")
 def index():
     return render_template("hello.html")
+
 @app.route("/" , methods=['GET', 'POST'])
 def upload_file():
     f= request.files.get("file1")
     uploaded_audio = app.config["UPLOAD_FOLDER"]+"/temp"
     f.save(uploaded_audio)
-    rendered_image_relative_path = run_file_in_trained_model(uploaded_audio)
-    rendered_image_absolute_path = os.path.join(PARENTDIR , rendered_image_relative_path)
-
-    return render_template("process.html", image = rendered_image_absolute_path)
+    output = run_file_in_trained_model(uploaded_audio)
+    postprocess.show_predicted_image(output)
+    return render_template("process.html", image = os.path.join(app.static_url_path,"new_image.png"))
 
 def run_file_in_trained_model(uploaded_audio_path):
+    """Returns base64 string.. representation of image
+
+    Args:
+        uploaded_audio_path (str): path to uploaded audio
+
+    Returns:
+        base64 string: decode for png/ jpg etc.
+    """
     loaded_model = pickle.load(open(MODEL, 'rb'))
     audio, sr = preprocess.loadaudio(uploaded_audio_path)
     audio_list = []
     audio_list.append(audio)
     audio_list_resized = preprocess.resizeaudio(audio_list,False)
     output = loaded_model.predict(audio_list_resized)
-    return postprocess.show_predicted_image(output)
+    return output
 
+@app.route('/<path:filename>')
+def download_file(filename):
+    try:
+        filename=os.path.join(app.root_path, filename)
+        return send_file(filename)
+    except FileNotFoundError:
+        abort(404)
--- model/postprocess.py
+++ model/postprocess.py
@@ -1,13 +1,12 @@
 import base64 as base64
-import matplotlib.pyplot as plt
 def show_predicted_image(y_predict):
     """Takes the base64 string output from the model and converts it to a image, 
-    then displays it
+    and writes in static/new_image.png
 
     Args:
         y_predict (ndarray): Shape is (1,), a single string contained within a np.array
     """
-    with open ("static/new_image.png","bw") as tempwrite:
+    with open ("application/static/new_image.png","bw") as tempwrite:
         tempwrite.write(base64.b64decode(y_predict))
-    return "static/new_image.png"
+   
     
